77ee5e912862632e807e2b85c7b0a35e930d69cf
student <student@virtualbox>
2016-10-05 12:46:04 +0200

    adding 455445

--- src/test/java/domain/Test3.java
+++ src/test/java/domain/Test3.java
@@ -5,7 +5,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Kangur");
+		System.out.println("Kangur5454545");
 		
 	}
 
