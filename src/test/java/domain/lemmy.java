ca8f6a220f6057d048b0be3caf6db56a92482da7
student <student@virtualbox>
2016-10-05 11:25:51 +0200

    deedd

--- src/test/java/domain/lemmy.java
+++ src/test/java/domain/lemmy.java
@@ -2,8 +2,14 @@ package domain;
 
 public class lemmy {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { 
+		//git push -uorigin "nazwa gałęzi"
+		// TODO Auto-generated method stub 
+		
+		
+		AppTest k = new AppTest(); 
+		
+		k.pisz();
 
 	}
 
